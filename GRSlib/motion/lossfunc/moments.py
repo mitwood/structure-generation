c0e3da6abf96aa346768139308165acb5d37d0b2
Mitchell Anthony Wood <mitwood@s992937.srn.sandia.gov>
2025-10-27 16:41:43 -0600

    Scoring mask for moments wasnt working, now is.

--- GRSlib/motion/lossfunc/moments.py
+++ GRSlib/motion/lossfunc/moments.py
@@ -75,7 +75,8 @@ class Moments(Scoring):
             self.mask[indices] = 1
             
         else:
-            self.mask = list(range(self.n_descriptors))
+            self.mask = np.zeros(self.n_descriptors, dtype=int) + 1
+
 
         if self.n_elements > 1:
             self.current_desc = self.current_desc.flatten()
@@ -119,8 +120,8 @@ class Moments(Scoring):
 
     @partial(jit, static_argnums=(0,))
     def first_moment(self, current_desc, target_desc):
-        current_avg = jnp.average(current_desc, axis=0)
-        target_avg = jnp.average(target_desc, axis=0)
+        current_avg = jnp.average(current_desc, axis=0)*self.mask
+        target_avg = jnp.average(target_desc, axis=0)*self.mask
         tst_residual = jnp.sum(jnp.nan_to_num(jnp.abs(current_avg-target_avg)))
         is_zero = jnp.array(jnp.isclose(tst_residual,jnp.zeros(tst_residual.shape)),dtype=int)
         bonus = -jnp.sum(is_zero*(float(self.config.sections['SCORING'].moments_bonus[0])))
@@ -129,8 +130,8 @@ class Moments(Scoring):
 
     @partial(jit, static_argnums=(0,))
     def second_moment(self, current_desc, target_desc):
-        current_std = jnp.std(current_desc, axis=0)
-        target_std = jnp.std(target_desc, axis=0)
+        current_std = jnp.std(current_desc, axis=0)*self.mask
+        target_std = jnp.std(target_desc, axis=0)*self.mask
         tst_residual = jnp.sum(jnp.nan_to_num(jnp.abs(current_std-target_std)))
         is_zero = jnp.array(jnp.isclose(tst_residual,jnp.zeros(tst_residual.shape)),dtype=int)
         bonus = -jnp.sum(is_zero*float(self.config.sections['SCORING'].moments_bonus[1]))
@@ -140,12 +141,12 @@ class Moments(Scoring):
     @partial(jit, static_argnums=(0,))
     def third_moment(self, current_desc, target_desc):
         #Showing my work for Pearsons skew = (3(mean-median)/stdev))
-        current_avg = jnp.average(current_desc, axis=0)
-        target_avg = jnp.average(target_desc, axis=0)
-        current_std = jnp.std(current_desc, axis=0)
-        target_std = jnp.std(target_desc, axis=0)
-        current_med = jnp.median(current_desc, axis=0)
-        target_med = jnp.median(target_desc, axis=0)
+        current_avg = jnp.average(current_desc, axis=0)*self.mask
+        target_avg = jnp.average(target_desc, axis=0)*self.mask
+        current_std = jnp.std(current_desc, axis=0)*self.mask
+        target_std = jnp.std(target_desc, axis=0)*self.mask
+        current_med = jnp.median(current_desc, axis=0)*self.mask
+        target_med = jnp.median(target_desc, axis=0)*self.mask
 
         current_skew = 3.0*(current_avg-current_med)/current_std
         target_skew = 3.0*(target_avg-target_med)/target_std
@@ -159,10 +160,10 @@ class Moments(Scoring):
     @partial(jit, static_argnums=(0,))
     def fourth_moment(self, current_desc, target_desc):
         #Showing my work for Kurtosis = Avg(z^4.0)-3 where z=(x-avg(x))/stdev(x)
-        current_avg = jnp.average(current_desc, axis=0)
-        target_avg = jnp.average(target_desc, axis=0)
-        current_std = jnp.std(current_desc, axis=0)
-        target_std = jnp.std(target_desc, axis=0)
+        current_avg = jnp.average(current_desc, axis=0)*self.mask
+        target_avg = jnp.average(target_desc, axis=0)*self.mask
+        current_std = jnp.std(current_desc, axis=0)*self.mask
+        target_std = jnp.std(target_desc, axis=0)*self.mask
 
         current_kurt = jnp.average(((current_desc-current_avg)/current_std)**4.0)-3.0 
         target_kurt = jnp.average(((target_desc-target_avg)/target_std)**4.0)-3.0 
